6ffcd9a7bfb2725e0871fd3a2ea583cde195f367
manageryzy <manageryzy@gmail.com>
2018-07-09 10:29:19 +0800

    init mod
    Signed-off-by: manageryzy <manageryzy@gmail.com>

---
+++ src/main/java/net/mite/MITE.java
@@ -0,0 +1,34 @@
+package net.mite;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by manageryzy on 7/6/2018.
+ */
+@Mod(modid = MITE.MODID, name = MITE.NAME, version = MITE.VERSION)
+public class MITE {
+    public static final String MODID = "mite";
+    public static final String NAME = "MITE is too easy";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
